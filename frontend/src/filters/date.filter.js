11c8469e11d2db19aba28289347fa65ee8efe91d
asosunoff <al.sosunoff@gmail.com>
2020-03-14 17:06:17 +0300

    create myself filter for datetime

---
+++ frontend/src/filters/date.filter.js
@@ -0,0 +1,27 @@
+export default function dateFilter(value, format = 'date') {
+	const options = {};
+
+	if (format.includes('date')) {
+		options.day = '2-digit';
+		options.month = 'long';
+		options.year = 'numeric';
+	}
+
+	if (format.includes('time')) {
+		options.hour = '2-digit';
+		options.minute = '2-digit';
+		options.second = '2-digit';
+	}
+
+	/* {
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+		second: 'numeric',
+		hour12: false,
+	} */
+
+	return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value));
+}
