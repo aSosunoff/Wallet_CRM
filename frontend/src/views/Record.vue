8a44b4fa6acf8063f374b259a98923e363cf7d40
asosunoff <al.sosunoff@gmail.com>
2020-03-22 11:15:35 +0300

    init categories on record page

--- frontend/src/views/Record.vue
+++ frontend/src/views/Record.vue
@@ -4,10 +4,19 @@
 			<h3>Новая запись</h3>
 		</div>
 
-		<form class="form">
+		<Loader v-if="loading" />
+
+		<form class="form" v-else>
 			<div class="input-field">
-				<select>
-					<option>name cat</option>
+				<select ref="select">
+					<option value="" disabled selected>Выбирите категорию</option>
+
+					<option
+						v-for="category of GET_CATEGORIES"
+						:key="category.id"
+						:value="category.id"
+						>{{ category.title }}</option
+					>
 				</select>
 				<label>Выберите категорию</label>
 			</div>
@@ -45,3 +54,35 @@
 		</form>
 	</div>
 </template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+	name: 'record',
+	data: () => ({
+		init_select: null,
+		loading: true,
+	}),
+	computed: {
+		...mapGetters(['GET_CATEGORIES']),
+	},
+	methods: {
+		...mapActions(['GET_ALL_CATEGORY']),
+	},
+	async mounted() {
+		await this.GET_ALL_CATEGORY();
+
+		this.loading = false;
+
+		await this.$nextTick();
+
+		this.init_select = window.M.FormSelect.init(this.$refs.select);
+	},
+	destroyed() {
+		if (this.init_select && this.init_select.destroy) {
+			this.init_select.destroy();
+		}
+	},
+};
+</script>
